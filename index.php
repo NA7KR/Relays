63a5e110ec8aab0f73bdf86503d5b8331c3a2a72
Kevin <kevin@na7kr.us>
2016-02-09 10:07:21 -0800

    login error

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
-include('login.php'); // Includes Login Script
+require_once("connection.php");  	//include your connection to your db
+	require_once("sqli.php");			//our file which holds our sql functions 
+    session_start(); // Starting Session
+    $error=''; // Variable To Store Error Message
+    if (isset($_POST['submit'])) 
+    {
+        if (empty($_POST['username']) || empty($_POST['password'])) 
+        {
+                $error = "Username or Password is invalid";
+        }
+        else
+        {
+            // Define $username and $password
+            $username=$_POST['username'];
+            $password=$_POST['password'];
+            
+            // To protect MySQL injection for Security purpose
+            $username = stripslashes($username);
+            $password = stripslashes($password);
+            $username = mysqli_real_escape_string($connection,$username);
+            $password = mysqli_real_escape_string($connection,$password);
+            // Selecting Database
+          
+            $query = query("select * from login where password='$password' AND username='$username'");
+            $rows = get_num_rows($query);
+            if ($rows == 1) {
+                    $_SESSION['login_user']=$username; // Initializing Session
+                    header("location: profile.php"); // Redirecting To Other Page
+                    } 
+                    else 
+                    {
+                            $error = "Username or Password is invalid";	
+                    }              
+        }
+    }
 
 if(isset($_SESSION['login_user'])){
 header("location: profile.php");
@@ -16,7 +50,7 @@ header("location: profile.php");
             <h1>Login</h1>
             <div id="login">
                 <h2>Login Form</h2>
-                <form action="login.php" method="post">
+                <form action="index.php" method="post">
                     <label>UserName :</label>
                     <input id="name" name="username" placeholder="username" type="text">
                     <label>Password :</label>
