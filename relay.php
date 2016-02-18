793e62f2ec35c5fdc560297c80623a0e5cd74e42
Kevin <kevin@na7kr.us>
2016-02-17 22:17:04 -0800

    split files

---
+++ relay.php
@@ -0,0 +1,62 @@
+<?php
+/******************
+	Save Names
+	******************/
+	function savepostrealynames()
+	{
+		$msg = updatenames(	$_POST["relay1"],
+							$_POST["relay2"],
+							$_POST["relay3"],
+							$_POST["relay4"],
+							$_POST["relay5"],
+							$_POST["relay6"],
+							$_POST["relay7"],
+							$_POST["relay8"]);
+		
+		return  $msg;
+	}
+	
+	/******************
+	Show Names
+	******************/
+	function showrelaynames()
+	{
+		$name_row = querysql("SELECT `Relay 1`,`Relay 2`,`Relay 3`,`Relay 4`,`Relay 5`,`Relay 6`,`Relay 7`,`Relay 8` FROM `Names`");
+		
+		$msg = "<table>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 1</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay1\" value=\"" . $name_row['Relay 1'] . "\" ></td>\n";	
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 2</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay2\" value=\"" . $name_row['Relay 2'] . "\" ></td>\n";		
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 3</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay3\" value=\"" . $name_row['Relay 3'] . "\" ></td>\n";	
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 4</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay4\" value=\"" . $name_row['Relay 4'] . "\" ></td>\n";		
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 5</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay5\" value=\"" . $name_row['Relay 5'] . "\" ></td>\n";		
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 6</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay6\" value=\"" . $name_row['Relay 6'] . "\" ></td>\n";	
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 7</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay7\" value=\"" . $name_row['Relay 7'] . "\" ></td>\n";	
+		$msg  .=  "	</tr>";
+		$msg  .=  "	<tr>";
+		$msg  .=  "		<td>Realy 8</td>";
+		$msg  .=  "<td><input type=\"text\" name=\"relay8\" value=\"" . $name_row['Relay 8'] . "\" ></td>\n";	
+		$msg  .=  "	</tr>";
+		$msg  .=  "	</table>";
+		return  $msg;
+	}
+?>
---
+++ user.php
@@ -0,0 +1,16 @@
+<?php
+	function showuser_rows($rows)
+	{
+		$msg = "<select name=\"users\" onchange=\"showUser(this.value)\">";
+		$msg .= "<option value=\"\">Select a person:</option>";
+		foreach($rows as $access_row) : 
+			$msg .=  "<option value=\"" . $access_row['id'] . "\">" . $access_row['username'] . "</option>";
+		endforeach;
+		$msg  .=  "	</select>";
+		$msg .="<div id=\"txtHint\"><b>Person info will be listed here...</b></div>";
+		return $msg;
+	}
+	
+	
+	
+?>
